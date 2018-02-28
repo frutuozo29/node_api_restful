4a437195471015c4c033dbd7a94b08ac927c5b60
Renan Frutuozo <frutuozo29@gmail.com>
2018-02-27 23:28:16 -0300

    Metodo update da rota product

--- api/routes/products.js
+++ api/routes/products.js
@@ -5,9 +5,19 @@ const mongoose = require('mongoose');
 const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling GET request to /products'
-    });
+    Product.find()
+        .then(products => {
+            console.log(products);
+            // if (products.length >= 0) {
+            res.status(200).json(products);
+            // } else {
+            //     res.status(404).json({ message: 'Not found' })
+            // }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: err });
+        });
 });
 
 router.post('/', (req, res, next) => {
@@ -33,7 +43,11 @@ router.get('/:productId', (req, res, next) => {
     Product.findById(id)
         .then(doc => {
             console.log(doc);
-            res.status(200).json(doc);
+            if (doc) {
+                res.status(200).json(doc);
+            } else {
+                res.status(404).json({ message: 'Not found' });
+            }
         })
         .catch(err => {
             console.log(err);
@@ -42,15 +56,32 @@ router.get('/:productId', (req, res, next) => {
 });
 
 router.patch('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Updated produtc!'
-    })
+    const id = req.params.productId;
+    const updateOps = {};
+    for (const ops of req.body) {
+        updateOps[ops.propName] = ops.value;
+    }
+    Product.update({ _id: id }, { $set: updateOps })
+        .then(res => {
+            console.log(res);
+            req.status(200).json(res);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: err });
+        });
 });
 
 router.delete('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Deleted produtc!'
-    })
+    const id = req.params.productId;
+    Product.remove({ _id: id })
+        .then(product => {
+            res.status(200).json(product);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: err });
+        });
 });
 
 module.exports = router;
