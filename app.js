ba6e8ddcd12d8b2aeb99e279064380f0dbd75a05
Renan Frutuozo <frutuozo29@gmail.com>
2018-02-27 21:19:09 -0300

    Adicionando mongodb atlas e configurando nodemon

--- app.js
+++ app.js
@@ -2,19 +2,21 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+mongoose.connect('mongodb+srv://node-api:' + process.env.MONGO_ATLAS_PW + '@node-api-restful-mpvhl.mongodb.net/test');
+
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
     res.header('Acess-Control-Allow-Origin', '*');
     res.header('Acess-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    
+
     if (req.method === 'OPTIONS') {
         res.header('Acess-Control-Allow-Methods', 'PUT POST, PATCH, DELETE, GET');
         return res.status(200).json({});
